03edcd4b98a8570d393c2759b14432edd67d23f9
Jaco Kok <kokjaco2@gmail.com>
2025-09-12 15:30:29 +0200

    feat: update styles and layout with new theming and UI components
    - Enhanced app.css with custom color variables and dark mode support.
    - Integrated ModeWatcher from @kayord/ui for theme management in layout.
    - Added layout.ts to disable SSR and prerendering.
    - Updated test page to include Button and ThemeSelector components.
    - Configured svelte.config.js for aliasing and fallback settings.

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import { ModeWatcher } from "@kayord/ui/mode-watcher";
 
 	let { children } = $props();
 </script>
@@ -9,4 +10,5 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<ModeWatcher defaultMode="dark" />
 {@render children?.()}
--- client/src/routes/test/+page.svelte
+++ client/src/routes/test/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { Button, ThemeSelector } from "@kayord/ui";
 	const handleLoginWithGoogle = () => {
 		window.location.href =
-			'http://localhost:5017/account/login/google?returnUrl=http://localhost:5173';
+			"http://localhost:5017/account/login/google?returnUrl=http://localhost:5173";
 	};
 </script>
 
 <div>+page</div>
 <button onclick={handleLoginWithGoogle}>Login</button>
+
+<Button>Test</Button>
+<ThemeSelector />
